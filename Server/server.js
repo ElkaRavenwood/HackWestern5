2854461669f044efbcf295fdf88dbc8c41b75778
Saiyan-God <sagarpatel1313@gmail.com>
2018-11-24 08:22:08 -0500

    Committing files to load web service

---
+++ Server/server.js
@@ -0,0 +1,45 @@
+var express = require('express'),
+  app = express(),
+  port = process.env.PORT || 3000,
+  bodyParser = require('body-parser');
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+var mysql = require('mysql');
+
+var con = mysql.createConnection({
+  host: "35.192.48.135",
+  user: "root",
+  password: "YfYjx7SKMdXVHtdd",
+  database: "test"
+});
+
+con.connect();
+
+app.get('/', async function(req, res) {
+
+
+	var acr = req.query.acr;
+	var meaning = [];
+	var returnVal = {};
+	var q = 'SELECT * FROM Acr WHERE Acronym="' + acr + '"';
+	con.query(q, function(err, rows, fields) {
+		if (err) throw err;
+
+		for(var i = 0; i < rows.length ; i++){
+			meaning.push(rows[i]['Full']);
+		}
+		returnVal[acr] = meaning;
+		//res.send(returnVal);
+		res.json(JSON.stringify(returnVal));
+	});
+});
+
+/*
+var routes = require('./api/routes/todoListRoutes'); //importing route
+routes(app); //register the route
+*/
+app.listen(port);
+
+console.log('todo list RESTful API server started on: ' + port);
