461786f5c9adfe3013b9612c2c1d91c865a33020
Jamie W <jamie.won.2000@gmail.com>
2018-11-25 00:34:28 -0500

    Details opens website, Manifest editted to exclude our website (looks ugly otherwise), userToArray handles multiple tables in databas

--- userToArray.js
+++ userToArray.js
@@ -1,5 +1,74 @@
 const words = new Set(); // List of words
 let acrArray = []; // List of acronyms
+var tableNames = ['medicalData', 'techData', 'armyData', 'slangData','businessData']; // For table names
+
+let initPanel = document.createElement("div");
+initPanel.id = "initPanel";
+initPanel.appendChild(document.createTextNode("Click the database you want to use!\n"));
+initPanel.style.textAlign = "center";
+initPanel.style.alignItems = "center";
+
+let newBod = document.createElement("div");
+newBod.id = "newBod";
+newBod.style.maxWidth = "80%";
+newBod.style.paddingLeft = "20px";
+newBod.style.overflowY = "scroll";
+newBod.innerHTML  = document.body.innerHTML;
+newBod.style.position = "-webkit-sticky";
+newBod.style.position = "sticky";
+document.body.innerHTML = "";
+
+for (let i = 0; i < tableNames.length; i ++) {
+	let tableBut = document.createElement("button");
+	let tableText = document.createTextNode(tableNames[i].substring(0,tableNames[i].indexOf("Data")));
+	tableBut.appendChild(tableText);
+	tableBut.style.margin = "10px";
+	tableBut.style.padding = "10px";
+	tableBut.onclick = function () {findTable(tableNames[i]);}
+	initPanel.appendChild(tableBut);
+}
+
+// Inserts into document
+document.body.appendChild(initPanel);
+document.body.appendChild(newBod);
+
+function findTable (tableName) {
+	
+	// Converts user details into array
+	userToArray(document.body);
+
+	// Creates div element, sets style
+	let div = document.createElement("div");
+	div.id = "defList";
+	div.style.padding = "20px";
+	div.style.width = "20%";
+	div.style.height = screen.height;
+	div.style.overflowX = "scroll";
+	div.style.overflowY = "scroll";
+	div.style.backgroundColor = "#f5fffa";
+	div.style.fontFamily = document.getElementById("newBod").style.fontFamily;
+	div.style.fontSize = document.getElementById("newBod").fontSize;
+	div.style.fontColor = "black";
+	div.style.position = "-webkit-sticky";
+	div.style.position = "sticky";
+	document.body.style.display = "flex";
+	document.body.style.float = "top";
+
+	// Gets acronyms
+	checkAcr(Array.from(words), tableName);
+
+	// Resets html
+	document.body.innerHTML = "";
+
+	// Inserts into document
+	document.body.insertAdjacentElement('afterBegin', div);
+	document.body.insertAdjacentElement('beforeEnd', newBod);
+
+	console.log('end');
+
+}
+
+
 
 // Collects all words in a web page
 function userToArray (node) {
@@ -61,7 +130,7 @@ function userToArray (node) {
 	}
 }
 
-function checkAcr(webpage){
+function checkAcr(webpage,tableName){
    var acrArray = []
    for(var i = 0; i < webpage.length; i++){
        var acrWord = false;
@@ -75,7 +144,7 @@ function checkAcr(webpage){
                acrWord = true;
            }
        }
-
+ 
        if(acrWord){
            for(z = 0; z < word.length; z++){
                word = word.replace('.', '');
@@ -86,67 +155,33 @@ function checkAcr(webpage){
 
    }
    for(var i = 0; i < acrArray.length; i++){
-		getAcronyms(acrArray[i]);
+		getAcronyms(acrArray[i], tableName);
 	}
 }
 
-// Converts user details into array
-userToArray(document.body);
+function getAcronyms(acr, tableName) {
 
-// Gets acronyms
-checkAcr(Array.from(words));
-
-// Creates div element, sets style
-let div = document.createElement("div");
-div.id = "defList";
-div.style.padding = "20px";
-div.style.width = "20%";
-div.style.height = screen.height;
-div.style.overflowX = "scroll";
-div.style.overflowY = "scroll";
-div.style.backgroundColor = "#f5fffa";
-div.style.fontFamily = document.body.style.fontFamily;
-div.style.fontSize = document.body.style.fontSize;
-div.style.fontColor = "black";
-div.style.position = "-webkit-sticky";
-div.style.position = "sticky";
-document.body.style.display = "flex";
-document.body.style.float = "top";
-
-// Creates new body element, sets style
-let newBod = document.createElement("div");
-newBod.id = "newBod";
-newBod.style.maxWidth = "80%";
-newBod.style.paddingLeft = "20px";
-newBod.style.overflowY = "scroll";
-newBod.innerHTML  = document.body.innerHTML;
-newBod.style.position = "-webkit-sticky";
-newBod.style.position = "sticky";
-document.body.innerHTML = "";
-// Inserts into document
-document.body.insertAdjacentElement('afterBegin', div);
-document.body.insertAdjacentElement('beforeEnd', newBod);
-
-
-console.log('end');
-
-function getAcronyms(acr) {
-	console.log("Test - " + acr);
   var xhttp = new XMLHttpRequest();
   xhttp.onreadystatechange = function() {
+
     if (this.readyState == 4 && this.status == 200) {
-    	//console.log(JSON.parse(this.responseText));
+    	console.log(JSON.parse(this.responseText));
     	updateAcronyms(JSON.parse(this.responseText));
     }
   };
-  xhttp.open("GET", "http://localhost:3000/?acr="+acr, true);
+
+  xhttp.open("GET", "http://localhost:3000/?acr="+acr + "+table="+tableName, true);
+
   xhttp.send();
+
 }
 
 function updateAcronyms(acrDict){
+
 	var key = acrDict[0];
 
 	for(var i = 1; i < acrDict.length; i++){
+		console.log(9);
 		if(acrDict[i] != ""){
 			let par = document.createElement("p");
 			let text = document.createTextNode(key + " - " + acrDict[i] + "\n");
@@ -156,3 +191,4 @@ function updateAcronyms(acrDict){
 	}
 	
 }
+
